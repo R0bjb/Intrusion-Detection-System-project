8757a43aa9353bfeb299c4fdda1a5329ce1584af
Robert Blakeley <robjb04@gmail.com>
2026-02-24 13:41:20 +0000

    created a rough outline of the packet capturing method, this should be adapted in the future

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import csv, time, os, sys
+from scapy.all import sniff, wrpcap, IP, IPv6, TCP, UDP, conf
 
-# Press F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+OS = os.name
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+iface = conf.iface
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+f = open("packets.csv", "a", newline="")
+w = csv.writer(f)
+""" This code saves the data into a csv file """
+def handle(packet):
+    ts = time.time()
+    ipver = None
+    src = dst = proto = sport = dport = tcp_flags = None
+    length = len(packet)
+    if IP in packet:
+        ipver = 4
+        src, dst = packet[IP].src, packet[IP].dst
+        proto = packet[IP].proto
+    elif IPv6 in packet:
+        ipver = 6
+        src, dst = packet[IPv6].src, packet[IPv6].dst
+        proto = packet[IPv6].nh
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if TCP in packet:
+        sport, dport = packet[TCP].sport, packet[TCP].dport
+        tcp_flags = int(packet[TCP].flags)
+    elif UDP in packet:
+        sport, dport = packet[UDP].sport, packet[UDP].dport
+
+    w.writerow([ts, ipver, src, dst, proto, sport, dport, length, tcp_flags])
+    f.flush()
+
+
+sniff(prn=handle, store=False, filter="ip or ip6")
+
+    
+#stores 20 packets
+packets = sniff(count=20, store = True)
+wrpcap("PcapTest.pcap", packets)
+
+
+
+
+#prints out the summary of captured
+packets.nsummary()
